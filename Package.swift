6415fb3597a0b7cdfb9b0c0be71f909076aaa7c6
CuongApero <cuongnd@apero.vn>
2026-05-08 18:00:09 +0700

    release: OnboardFlowKit v1.2.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "OnboardFlowKit",
             url: "https://github.com/AperoVN/SupermindOnboardSwift-Distribution/releases/download/1.2.0/OnboardFlowSDK-1.2.0.zip",
-            checksum: "77f1e900e75181565962c6e4466d57fcb062670910738d4bc08693c30a3d5025"
+            checksum: "c0ee53e29a6d76aa8b9564a8c6a5c9ea596c2769210c489eef157859a4220d9b"
         )
     ]
 )
