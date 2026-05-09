5ce9e3d944b96a74a6594a47d75a84eadb4aa8db
CuongApero <cuongnd@apero.vn>
2026-05-09 14:23:08 +0700

    release: OnboardFlowKit v1.3.5 (static + deps)

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/divkit/divkit-ios", from: "32.0.0"),
-        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", from: "5.0.0"),
-        .package(url: "https://github.com/AperoVN/SupermindCMSSwift-Distribution", from: "1.0.2")
+        .package(url: "https://github.com/divkit/divkit-ios", exact: "32.48.0"),
+        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", exact: "5.72.0"),
+        .package(url: "https://github.com/AperoVN/SupermindCMSSwift-Distribution", exact: "1.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "OnboardFlowKitBinary",
-            url: "https://github.com/AperoVN/SupermindOnboardSwift-Distribution/releases/download/1.3.4/OnboardFlowSDK-1.3.4.zip",
-            checksum: "a4f7ce336491c9c9ffa0c49ee573d5ba496156ae15989180d76f334d9e2f7e6c"
+            url: "https://github.com/AperoVN/SupermindOnboardSwift-Distribution/releases/download/1.3.5/OnboardFlowSDK-1.3.5.zip",
+            checksum: "96a3e7c1ca5ff8f8754bbe0834e9b633928fd75decd4a33551eb172692487154"
         ),
         .target(
             name: "OnboardFlowKitTarget",
