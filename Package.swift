29285b9bb0111c674370dbe1c5532f543a5939a7
CuongApero <cuongnd@apero.vn>
2026-05-09 10:19:07 +0700

    release: OnboardFlowKit v1.3.1 (static + deps)

--- Package.swift
+++ Package.swift
@@ -10,14 +10,29 @@ let package = Package(
     products: [
         .library(
             name: "OnboardFlowKit",
-            targets: ["OnboardFlowKit"]
+            targets: ["OnboardFlowKitTarget"]
         )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/divkit/divkit-ios", from: "32.0.0"),
+        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", from: "5.0.0"),
+        .package(url: "https://github.com/AperoVN/SupermindCMSSwift-Distribution", from: "1.0.2")
     ],
     targets: [
         .binaryTarget(
-            name: "OnboardFlowKit",
+            name: "OnboardFlowKitBinary",
             url: "https://github.com/AperoVN/SupermindOnboardSwift-Distribution/releases/download/1.3.1/OnboardFlowSDK-1.3.1.zip",
-            checksum: "4ec72a731693e21029f9b67134e48a35d7adccc2bcf3a2c65a59e3a8ef57e104"
+            checksum: "013f5dbdea0a488a1f1ee68465ca980fe99e31d53ef4f6628a7ea623577c22f0"
+        ),
+        .target(
+            name: "OnboardFlowKitTarget",
+            dependencies: [
+                "OnboardFlowKitBinary",
+                .product(name: "DivKit", package: "divkit-ios"),
+                .product(name: "RevenueCatUI", package: "purchases-ios-spm"),
+                .product(name: "SupermindCMSSwift", package: "SupermindCMSSwift-Distribution")
+            ],
+            path: "Sources/OnboardFlowKitTarget"
         )
     ]
 )
---
+++ Sources/OnboardFlowKitTarget/Exports.swift
@@ -0,0 +1,4 @@
+// Re-export the OnboardFlowKit binary module so consumers
+// can `import OnboardFlowKit` and get all public API +
+// @_exported RevenueCat / SupermindCMSSwift types.
+@_exported import OnboardFlowKit
