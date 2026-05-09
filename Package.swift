c1969e2af7f4d76e07ee6251eee09519d5cc040d
CuongApero <cuongnd@apero.vn>
2026-05-09 17:17:45 +0700

    release: OnboardFlowKit v1.3.8 (static + deps)

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/RevenueCat/purchases-ios-spm", exact: "5.72.0"),
-        .package(url: "https://github.com/AperoVN/SupermindCMSSwift-Distribution", exact: "1.0.0")
+        .package(url: "https://github.com/AperoVN/SupermindCMSSwift-Distribution", exact: "1.0.2")
     ],
     targets: [
         .binaryTarget(
             name: "OnboardFlowKitBinary",
-            url: "https://github.com/AperoVN/SupermindOnboardSwift-Distribution/releases/download/1.3.7/OnboardFlowSDK-1.3.7.zip",
-            checksum: "b461fc5f360fa534b86b32f3e3e0c8e510932d88e33d436d9147e839c1344434"
+            url: "https://github.com/AperoVN/SupermindOnboardSwift-Distribution/releases/download/1.3.8/OnboardFlowSDK-1.3.8.zip",
+            checksum: "77f855d8627a2ec0c2569f79a7c8d4827b47e6cab9ebb7b6b4e5bc3f7b4db4d2"
         ),
         .target(
             name: "OnboardFlowKitTarget",
